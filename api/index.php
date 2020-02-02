9cf1131097dc983067ea7db026d5ad1c6038d5bf
Jeppevinkel <jeppevinkel@gmail.com>
2020-02-02 16:26:32 +0100

    Changed tab title of the api index

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-    <title>Southwood - Error</title>
+    <title>Southwood - API</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/error.css" type="text/css" rel="stylesheet" media="screen,projection">
     <link rel="icon" href="/images/logo.ico">
