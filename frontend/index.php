abd0e3d14c345f5033e88bc71351fd818c84c508
Jeppevinkel <jeppevinkel@gmail.com>
2020-02-02 14:47:05 +0100

    Added under contruction page

--- frontend/index.php
+++ frontend/index.php
@@ -1,6 +1,18 @@
-<?php
+<!doctype html>
+<title>Southwood Studios</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
 
-echo 'This is where we should make the kigen style list';
-echo 'auto deploy test 3';
-
-?>
+<article>
+    <h1>Under construction!</h1>
+    <div>
+        <p>This page is still being built, in the meantime you can still use our <a href="api.southwoodstudios.com">API</a>. If you want to come with suggestions, or just hang, you can find us in the <a href="https://discord.gg/2vFj4EY">EXILED</a> Discord, or in our <a href="https://discord.gg/9SWkAAt">Southwood Studios</a> Discord!</p>
+        <p>&mdash; Southwood Studios</p>
+    </div>
+</article>
